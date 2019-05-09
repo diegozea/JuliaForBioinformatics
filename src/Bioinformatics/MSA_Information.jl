5632622736f134009974ab7d73b1e3ce8649c6c9
Diego Javier Zea <diegozea@gmail.com>
2019-05-09 18:17:59 +0200

    update

--- src/Bioinformatics/MSA_Information.jl
+++ src/Bioinformatics/MSA_Information.jl
@@ -1,3 +1,100 @@
+# # Counting residues
+#
+# MIToS was designed to perform fast counting of residues. To achieve that,
+# each `Residue` is encoded as an integer that can be used to index.
+#
+
+using MIToS.MSA
+res = Residue('C')
+
+#-
+
+Int(res)
+
+# Let's count residues in the *UBA domain* Pfam family.
+
+using MIToS.Pfam
+const msa_file = downloadpfam("PF16577")
+
+# -
+
+const msa = read(msa_file, Stockholm)
+
+#-
+
+col_i = msa[:, 10]
+
+#-
+
+using MIToS.Information
+
+#-
+
+Ni = count(col_i)
+
+#-
+
+Pi = probabilities(col_i)
+
+#-
+
+Pi.table
+
+#-
+
+Pi[Residue('P')]
+
+#-
+
+col_j = msa[:, 15]
+
+Nij = count(col_i, col_j)
+
+#-
+
+Nij[Residue('P'), Residue('R')]
+
+#-
+
+Pij = normalize(Nij.table)
+
+#-
+
+Pij[Residue('P'), Residue('R')]
+
+#-
+
+using Plots
+plotlyjs()
+
+#-
+
+x = string.(Residue.(UngappedAlphabet()))
+
+#-
+
+clibraries()
+
+#-
+
+showlibrary(:Plots)
+
+#-
+
+clibrary(:cmocean)
+
+#-
+
+heatmap(x, x, Nij, color=:tempo)
+
+#-
+
+entropy(Ni)
+
+#-
+
+mutual_information(Nij)
+
 # ### Conservation
 #
 # You can use the MIToS' Information module to calculate the Shannon entropy of
